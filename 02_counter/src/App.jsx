87d0418d65c42d439b07d5aff1b95803cf2ecfb3
ganeshdarewar <ganeshdarewar@gmail.com>
2024-04-05 15:46:06 +0530

    why we need hooks

---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // let counter =5
+
+  let [counter , setCount] = useState(0);
+  
+  const addValue = () =>{
+    if(counter<20){
+      counter++
+      setCount(counter)
+    }
+  }
+
+  const removeValue = () => {
+    if(counter>0){
+      setCount(prevCount => prevCount - 1);
+    }
+  }
+  
+  return (
+    <>
+    <h1>chai aur react</h1>
+    <h2>Counter Value :{counter}</h2>
+    <button
+    onClick={addValue}
+    >Add Value</button>
+    <button onClick={removeValue}>Decrease Value</button>
+    </>
+  )
+}
+
+export default App
