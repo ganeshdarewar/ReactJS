9e54dbb357ca9254878ed36417d8e983f9e60bf5
Ganesh Darewar <ganeshdarewar@gmail.com>
2024-06-13 12:56:51 +0530

    enviroment variables and appwrite setup

---
+++ 12_MegaBlog/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import conf from './conf/config';
+
+function App() {
+  // for create react app
+  // console.log(process.env.REACT_APP_APPWRITE_URL);
+
+  // for vite
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  console.log(conf);
+  return (
+    <></>
+  )
+}
+
+export default App
---
+++ 12_MegaBlog/src/conf/config.js
@@ -0,0 +1,7 @@
+const conf = {
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteCollectionID: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteBucketID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+};
+export default conf;
