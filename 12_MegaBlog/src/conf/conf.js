f53249c4a850c106342701218bed3c1a7c032b32
Ganesh Darewar <ganeshdarewar@gmail.com>
2024-06-15 12:18:26 +0530

    rename files

--- 12_MegaBlog/src/conf/conf.js
+++ 12_MegaBlog/src/conf/conf.js
@@ -1,6 +1,7 @@
 const conf = {
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
   appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
   appwriteCollectionID: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
   appwriteBucketID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 };
